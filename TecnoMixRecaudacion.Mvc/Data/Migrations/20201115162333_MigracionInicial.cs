37409c8169bbc16d2bfd03e809f462c891acd951
Ruben López <Ruben Lpez@DESKTOP-I7HESU3>
2020-11-15 11:47:03 -0500

    Commit innicial del proyecto TecnoMixRecaudacion.Mvc (Presentacion)

---
+++ TecnoMixRecaudacion.Mvc/Data/Migrations/20201115162333_MigracionInicial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TecnoMixRecaudacion.Mvc.Data.Migrations
+{
+    public partial class MigracionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
